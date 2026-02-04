8800cd7c7b90ddd1cd02034cc8621c0b027b60dd
JeuneKaiser <enzobiger@gmail.com>
2026-02-04 09:54:05 +0100

    preprocessing pandas no sckit learn

---
+++ preprocessing/preprocessing.py
@@ -0,0 +1,27 @@
+from json import load
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import ColumnTransfromer
+from sklearn.pipeline import Pipeline
+
+def load_data():
+    df = pd.read_csv('api/iris.csv')
+    print(df.head())
+    return df
+
+df = load_data()
+
+
+def basic_cleanning(df):
+    df.dropna()
+    df.drop_duplicates()
+    print(df.shape)
+    print()
+    return df
+
+
+
+
+
+
+
