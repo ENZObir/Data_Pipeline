5d04e288e90316934b52d87e6213577e43b47224
Seif-allah Fares <seif-allahfares@MacBook-Air-de-Seif-allah.local>
2026-02-05 10:35:30 +0100

    Avancement jour 2

---
+++ api/config.py
@@ -0,0 +1,6 @@
+MLFLOW_MODEL_PATH = "models/iris_model"  # à mettre à jour quand le vrai modèle est prêt
+POSTGRES_HOST = "postgres"
+POSTGRES_PORT = 5432
+POSTGRES_DB = "irisdb"
+POSTGRES_USER = "user"
+POSTGRES_PASSWORD = "password"
